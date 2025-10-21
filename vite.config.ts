445e0649270455b7e3f64fe8edbbf382c7a87fa0
JoWilks25 <58911477+JoWilks25@users.noreply.github.com>
2025-10-21 10:41:52 +0100

    Generate build for github pages deploy (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/d3-Contributor-Stats-Dashboard/', // Replace with your actual repo name
 })
